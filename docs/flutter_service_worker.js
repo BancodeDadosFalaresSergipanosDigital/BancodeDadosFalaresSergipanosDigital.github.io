95312c643c0a4af57c5729bb93527bdbda94b90a
KevennyJS <kevennykeke@gmail.com>
2022-07-01 15:48:43 -0300

    upload pagina

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -5,21 +5,22 @@ const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
-"assets/fonts/MaterialIcons-Regular.otf": "7e7a6cccddf6d7b20012a548461d5d81",
-"assets/NOTICES": "7b0bf9d7916c03f725159429279285bd",
+"assets/fonts/MaterialIcons-Regular.otf": "95db9098c58fd6db106f1116bae85a0b",
+"assets/NOTICES": "2dd201d5884aa116686da0a6b69c3f9a",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
 "canvaskit/canvaskit.js": "c2b4e5f3d7a3d82aed024e7249a78487",
 "canvaskit/canvaskit.wasm": "4b83d89d9fecbea8ca46f2f760c5a9ba",
 "canvaskit/profiling/canvaskit.js": "ae2949af4efc61d28a4a80fffa1db900",
 "canvaskit/profiling/canvaskit.wasm": "95e736ab31147d1b2c7b25f11d4c32cd",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
+"flutter.js": "eb2682e33f25cd8f1fc59011497c35f8",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "89acd788f79c0b86e0c886438070355d",
-"/": "89acd788f79c0b86e0c886438070355d",
-"main.dart.js": "099f6fc4c568e2531006f0478a36095e",
+"index.html": "0cabcedafe62986770f17c9980f152a0",
+"/": "0cabcedafe62986770f17c9980f152a0",
+"main.dart.js": "b86779b2ced38c114548f91efeb8f998",
 "manifest.json": "3ffdd8e1004935574f76a5e0d998071d",
 "version.json": "18fdc2f7c5bdd4367ab83e1ddb7aef43"
 };
@@ -27,8 +28,7 @@ const RESOURCES = {
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = [
-  "/",
-"main.dart.js",
+  "main.dart.js",
 "index.html",
 "assets/NOTICES",
 "assets/AssetManifest.json",
